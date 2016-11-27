68caaac7a14c61da1e4f1b6b2e0f58b158b8ee4d
Kaito Udagawa <umireon@gmail.com>
2016-11-27 19:59:24 +0900

    Fix the location of .vim/plugins.toml

--- recipes/00install-links.sh
+++ recipes/00install-links.sh
@@ -3,6 +3,6 @@
 ln -fs .dotfiles/.bash_profile $HOME/.bash_profile
 ln -fs .dotfiles/.editorconfig $HOME/.editorconfig
 ln -fs .dotfiles/.gemrc $HOME/.gemrc
-ln -fs .dotfiles/.vim/plugins.toml $HOME/plugins.toml
+ln -fs .dotfiles/.vim/plugins.toml $HOME/.vim/plugins.toml
 ln -fs .dotfiles/.vimrc $HOME/.vimrc
 
