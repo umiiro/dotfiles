1da3a763ebb1e9f85647c39372860efa17c06839
Kaito Udagawa <umireon@gmail.com>
2016-11-27 18:51:46 +0900

    Add Homebrew and minimal set of tools

---
+++ recipes/00install-homebrew-minimal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# vim: set et sw=4:
+
+export HOMEBREW_DEVELOPER=1
+
+if [ ! -x /usr/local/bin/brew ]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+fi
+
+brew install vim
+brew cask install coteditor
+brew cask install iterm2
+brew cask install lacaille
+brew cask install atom
+
